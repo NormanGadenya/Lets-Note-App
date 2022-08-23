cf951369eb383928e10baef84b9e9c04c9e19492
Ngadenya <ngadenya@gmail.com>
2022-08-23 16:14:07 +0300

    Terms and conditions

--- app/src/main/java/com/neuralbit/letsnote/TermsAndConditions.kt
+++ app/src/main/java/com/neuralbit/letsnote/TermsAndConditions.kt
@@ -1,15 +1,19 @@
 package com.neuralbit.letsnote
 
 import android.os.Bundle
-import android.widget.TextView
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
+
 
 class TermsAndConditions : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_terms_and_conditions)
 
-        val tCTV = findViewById<TextView>(R.id.termsAndConditionTV)
-        tCTV.setText("Terms and conditions")
+        val webView = findViewById<WebView>(R.id.termsAndConditionWV)
+        webView.loadUrl("https://www.app-privacy-policy.com/live.php?token=tstFlldVqoQF3FCgRzhhY1h9awm3uGgc")
+        supportActionBar?.title = "Terms and Conditions"
+        webView.webViewClient = WebViewClient()
     }
 }
