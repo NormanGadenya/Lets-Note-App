3db33d9dc52181c190768d94ca2197c1f23cbd1d
Ngadenya <ngadenya@gmail.com>
2022-09-29 16:22:25 +0300

    Added translation to German Label remove fix

--- app/src/main/java/com/neuralbit/letsnote/repos/LabelFireRepo.kt
+++ app/src/main/java/com/neuralbit/letsnote/repos/LabelFireRepo.kt
@@ -54,7 +54,7 @@ class LabelFireRepo {
 
     fun addOrDeleteLabels(newLabel : Int, oldLabel : Int, noteUid: String, labelTitle : String?, add : Boolean) {
         val labelRef = fUser?.let { database.getReference(it.uid).child("labels")}
-        if (newLabel>0){
+
             labelRef?.addListenerForSingleValueEvent( object  : ValueEventListener {
                 override fun onDataChange(s: DataSnapshot) {
                     var exists = false
@@ -63,7 +63,7 @@ class LabelFireRepo {
                         if (snapshot.key == newLabel.toString()){
                             if (labelFire != null) {
                                 val noteUids = labelFire.noteUids
-                                if (add) {
+                                if (add && newLabel > 0) {
                                     if (!noteUids.contains(noteUid)){
                                         noteUids.add(noteUid)
                                         val updateMap = HashMap<String, Any>()
@@ -100,7 +100,7 @@ class LabelFireRepo {
                             }
                         }
                     }
-                    if (!exists){
+                    if (!exists && add){
                         val newLabelMap = HashMap<String, Any>()
                         val noteUids = ArrayList<String>()
                         noteUids.add(noteUid)
@@ -117,7 +117,7 @@ class LabelFireRepo {
                     Log.d(TAG, "onCancelled: $error")
                 }
             })
-        }
+
 
     }
 
